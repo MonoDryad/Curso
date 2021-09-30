44ec25a56b0c871b161939ec263630a6cf59d1a6
Rai Raul <87578739+MonoDryad@users.noreply.github.com>
2021-09-29 22:54:29 -0300

    Aula 1

---
+++ Importante - Curso/CursoSenai/script1.js
@@ -0,0 +1,17 @@
+let value1 = 15
+let value2 = parseInt(Math.random() * 30)
+
+console.log(value1,value2)
+
+if(value1 >= value2){
+    console.log('Tudo bem?')
+
+}else{
+    console.log('Não')
+
+}
+
+let x = 13
+let y = 12
+let resultado = x+y
+console.log(resultado)
