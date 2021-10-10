76c3c12b335624c87599586e4a33b092fe955830
Rai Raul <87578739+MonoDryad@users.noreply.github.com>
2021-10-10 14:35:01 -0300

    Final

--- Projetocc/script1.js
+++ Projetocc/script1.js
@@ -1,5 +1,5 @@
 function jogar(){
-    var audio = new Audio('aud/Chinese Rap Full--lGFpyuuchg.webm')
+    var audio = new Audio('aud/video0-online-audio-converter_L0R7wUM.mp3')
 audio.addEventListener('canplaythrough', function() {
   audio.play()
 })
@@ -22,6 +22,8 @@ function nexta2(){
     var creditoAtual = document.getElementById('creditosSociais').value
         let creditos = parseFloat(creditoAtual) + parseFloat(15)
         document.getElementById('creditosSociais').value = creditos
+
+        
 }
 
 function nexta3(){
@@ -40,27 +42,36 @@ let pper3 = document.querySelector('.pergunta3')
 function nexta4(){
 
     let acertou = document.querySelector('.acertou-pergunta')
-    acertou.style.display = "none"
+        acertou.style.display = "none"
 
-let pper4 = document.querySelector('.pergunta4')
-    pper4.style.display = "block"
+    let pper4 = document.querySelector('.pergunta4')
+        pper4.style.display = "block"
 
     var creditoAtual = document.getElementById('creditosSociais').value
-    let creditos = parseFloat(creditoAtual) + parseFloat(15)
-    document.getElementById('creditosSociais').value = creditos
+        let creditos = parseFloat(creditoAtual) + parseFloat(15)
+        document.getElementById('creditosSociais').value = creditos
 }
 
 function resultadoA(){
 
     let acertou = document.querySelector('.acertou-pergunta')
-    acertou.style.display = "none"
+        acertou.style.display = "none"
 
-let pper5 = document.querySelector('.labelResultadoA')
-    pper5.style.display = "block"
+    let creditosS = document.querySelector('.pcreditos')
+        creditosS.style.display = "none"
+
+    let pper5 = document.querySelector('.labelResultadoA')
+        pper5.style.display = "block"
+
+    let r = document.querySelector('.labelResultadoAb')
+        r.style.display = "block"
+
+    let social = document.querySelector('.social')
+        social.style.top = "15%"
 
     var creditoAtual = document.getElementById('creditosSociais').value
-    let creditos = parseFloat(creditoAtual) + parseFloat(15)
-    document.getElementById('creditosSociais').value = creditos
+        let creditos = parseFloat(creditoAtual) + parseFloat(15)
+        document.getElementById('creditosSociais').value = creditos
 }
 
 function nextf2(){
@@ -141,14 +152,26 @@ function nexte4(){
 }
 
 function nextfailm(){
+
     let acertou = document.querySelector('.errou-muito-feio-pergunta')
         acertou.style.display = "none"
 
-    let pper2 = document.querySelector('.resultadoB')
-        pper2.style.display = "block"
+    let creditosS = document.querySelector('.pcreditos')
+        creditosS.style.display = "none"
 
-        var creditoAtual = document.getElementById('creditosSociais').value
-        let creditos = parseFloat(creditoAtual) - parseFloat(99999999999)
+    let pper5 = document.querySelector('.labelResultadoB')
+        pper5.style.display = "block"
+
+    let resul = document.querySelector('.labelResultadoBa')
+        resul.style.display = "block"
+
+    let prox = document.querySelector('.fim2')
+        prox.style.display = "block"
+
+    let social = document.querySelector('.social')
+        social.style.top = "15%"
+
+        let creditos = -999999999999
         document.getElementById('creditosSociais').value = creditos
 }
 
@@ -406,4 +429,81 @@ errou.style.display = "block"
 
 let acertoubtn = document.querySelector('.btnNextaP5')
 acertoubtn.style.display = "block"
+}
+
+function proximoFim() {
+    let creditos = document.getElementById('creditosSociais').value
+    if(creditos >= -6){
+
+    var creditoAtual = document.getElementById('creditosSociais').value
+    let creditos = parseFloat(creditoAtual) + parseFloat(15)
+        document.getElementById('creditosSociais').value = creditos
+
+    let fim = document.querySelector('.proximo-fim')
+        fim.style.display = "block"
+
+    let tirarc = document.querySelector('.labelResultadoA')
+        tirarc.style.display = "block"
+
+        let tirarb = document.querySelector('.fimTexto')
+        tirarb.style.display = "none"
+
+    let acertou = document.querySelector('.acertou-pergunta')
+        acertou.style.display = "none"
+
+    let creditosS = document.querySelector('.pcreditos')
+        creditosS.style.display = "none"
+
+    let r = document.querySelector('.labelResultadoAb')
+        r.style.display = "block"
+
+    let social = document.querySelector('.social')
+        social.style.top = "15%"
+
+    }else if(creditos <= -15) {
+    
+    let fim = document.querySelector('.proximo-fim')
+        fim.style.display = "block"
+
+    let acertou = document.querySelector('.errou-muito-feio-pergunta')
+        acertou.style.display = "none"
+
+    let creditosS = document.querySelector('.pcreditos')
+        creditosS.style.display = "none"
+
+    let pper5 = document.querySelector('.labelResultadoB')
+        pper5.style.display = "none"
+
+    let resul = document.querySelector('.labelResultadoBa')
+        resul.style.display = "none"
+
+    let prox = document.querySelector('.fim2')
+        prox.style.display = "none"
+
+    let social = document.querySelector('.social')
+        social.style.top = "15%"
+
+        let creditos = -999999999999
+        document.getElementById('creditosSociais').value = creditos
+    }
+}
+
+function fim2() {
+    let menu = document.querySelector('.container-menu')
+        menu.style.display = "block"
+
+    let sper1 = document.querySelector('.pergunta1')
+        sper1.style.display = "none"
+
+    let fim = document.querySelector('.proximo-fim')
+        fim.style.display = "none"
+
+    let social = document.querySelector('.social')
+        social.style.top = "90%"
+
+        let socialT = document.querySelector('.pcreditos')
+        socialT.style.display = "block"
+        
+    let creditos = 0
+        document.getElementById('creditosSociais').value = creditos
 }
--- Scripts-JS/teste.js
+++ Scripts-JS/teste.js
@@ -1,6 +1,3 @@
-let  audio3 =  
-
-for(let i = audio3;i < 4;i++){
-    i.play()
-    console.log(i)
-}
+let creditoAtual = -15
+let creditos = parseFloat(creditoAtual) - parseFloat(99999999)
+console.log(creditos)
