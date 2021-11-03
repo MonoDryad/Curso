10b8f8ab895d310d03ee4fe0e389c66de86a9fd0
Rai Raul <87578739+MonoDryad@users.noreply.github.com>
2021-11-03 17:46:12 -0300

    Switch Case - For

--- Exercicios/Aula-7-Lista-de-Exercicios/Exercicio-7/script.js
+++ Exercicios/Aula-7-Lista-de-Exercicios/Exercicio-7/script.js
@@ -5,7 +5,6 @@
 
 let select = document.querySelector('select')
 var paragrafo = document.querySelector('p')
-var paragrafo2 = document.querySelector('nome')
 
 select.addEventListener('change', setProduto)
 
