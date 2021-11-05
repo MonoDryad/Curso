1bd60196b6dc9c67cf3ce425f7b4bbecdf478e9c
Rai Raul <87578739+MonoDryad@users.noreply.github.com>
2021-11-04 22:23:00 -0300

    Aula

--- Exercicios/Aula-8-LE/exercicio1/facil/script.js
+++ Exercicios/Aula-8-LE/exercicio1/facil/script.js
@@ -1,23 +1,80 @@
 let select = document.querySelector('select')
-var paragrafo = document.querySelector('p')
+var pre = 0
 
 select.addEventListener('change', setProduto)
 
 function setProduto(){
     let opcao = select.value
+
     console.log(opcao)
 
-    if(opcao === 'coca'){
-            reg.textContext = 'Código de Origem: ' + '1'
-            desc.textContext = 'Descrição: Produto que vai fuder a sua vida!'
-        }else if(opcao === 'sprite'){
-            reg.textContext = 'Código de Origem: ' + '5'
-            desc.textContext = 'Descrição: Produto que vai fuder a sua vida mais ainda!'
-        }else if(opcao === 'redbull'){
-            reg.textContext = 'Código de Origem: ' + '7'
-            desc.textContext = 'Descrição: Produto que vai fuder a sua vida mais ainda!'
-        }else if(opcao === 'monster'){
-            reg.textContext = 'Código de Origem: ' + '0'
-            desc.textContext = 'Descrição: Produto que vai fuder a sua vida mais ainda!'
-        }
+    if(opcao === 'Nenhum'){
+        paragrafo.textContent = 'Escolha uma opção!'
+        nome.textContent = 'Nome:'
+        descrição.textContent = 'Descrição:'
+        cdo.textContent = 'Código de Origem:'
+        pre = 8
+    }else if(opcao === 'Shampoo'){
+        paragrafo.textContent = 'Você selecionou a opção: ' + opcao
+        nome.textContent = 'Nome: ' + opcao + ' de Cavalo'
+        descrição.textContent = 'Descrição: Shampoo perfeito para você'
+        cdo.textContent = 'Código de Origem: ' + '2'
+        pre = 2
+    }else if(opcao === 'Acessorios'){
+        paragrafo.textContent = 'Você selecionou a opção: ' + opcao
+        nome.textContent = 'Nome: ' + opcao + ' de Cachorro'
+        descrição.textContent = 'Descrição: Acessório para cachorro!'
+        cdo.textContent = 'Código de Origem: ' + '0'
+        pre = 0
+    }else if(opcao === 'Maquiagem'){
+        paragrafo.textContent = 'Você selecionou a opção: ' + opcao
+        nome.textContent = 'Nome: ' + opcao + ' para Ovelhas'
+        descrição.textContent = 'Descrição: Ovelhas maquiadas (bonecos)'
+        cdo.textContent = 'Código de Origem: ' + '1'
+        pre = 1
+    }else if(opcao === 'Relogios'){
+        paragrafo.textContent = 'Você selecionou a opção: ' + opcao
+        nome.textContent = 'Nome: ' + opcao + ' colorido'
+        descrição.textContent = 'Descrição: Relógio coloridinho para seu filho!'
+        cdo.textContent = 'Código de Origem: ' + '7'
+        pre = 7
+    }else if(opcao === 'Colar'){
+        paragrafo.textContent = 'Você selecionou a opção: ' + opcao
+        nome.textContent = 'Nome: ' + opcao + ' de Cavalo'
+        descrição.textContent = 'Descrição: Colar perfeito para você'
+        cdo.textContent = 'Código de Origem: ' + '4'
+        pre = 4
     }
+}
+
+function verificar(){
+    console.log(pre)
+    switch(pre){
+        case 1:
+            regiao.textContent = 'Região: Sul'
+        break;
+        case 2:
+            regiao.textContent = 'Região: Norte'
+        break;
+        case 3:
+            regiao.textContent = 'Região: Leste'
+        break;
+        case 4:
+            regiao.textContent = 'Região: Oeste'
+        break;
+        case 5:
+            regiao.textContent = 'Região: Nordeste'
+        break;
+        case 6:
+            regiao.textContent = 'Região: Sudeste'
+        break;
+        case 7:
+            regiao.textContent = 'Região: Centro-Oeste'
+        break;
+        case 0:
+            regiao.textContent = 'Região: Fora do País'
+        break;
+        default:
+            regiao.textContent = 'Não selecionado'
+    }
+}
