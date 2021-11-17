27d6b244cf68507a1b14cf78c95443fb9657ffbf
Rai Raul <87578739+MonoDryad@users.noreply.github.com>
2021-11-17 17:46:37 -0300

    Delete Exercícios UC - LP directory

--- Exercícios UC - LP/Aula-Marcos-1/exercicio1/script.js
+++
@@ -1,3 +0,0 @@
-function calcular(){
-    document.getElementById('resultado').textContent = parseFloat(document.getElementById('valorX').value) + parseFloat(document.getElementById('valorY').value)
-}
